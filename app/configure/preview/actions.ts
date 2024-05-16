da7d86b1c71f25cffad31e7941928d65b2765aae
Emmanuel Adebiyi <adebiyiemmanuel11@gmail.com>
2024-05-16 20:17:34 +0100

    changes to auth-callback & stripe action

--- app/configure/preview/actions.ts
+++ app/configure/preview/actions.ts
@@ -68,7 +68,7 @@ export const createCheckoutSession = async ({
   const stripeSession = await stripe.checkout.sessions.create({
     success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/thank-you?orderId=${order.id}`,
     cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/configure/preview?id=${configuration.id}`,
-    payment_method_types: ["card", "cashapp", "paypal", "us_bank_account", "mobilepay"],
+    payment_method_types: ["card", "cashapp", "us_bank_account"],
     mode: "payment",
     shipping_address_collection: { allowed_countries: ["US", "NG"] },
     metadata: {
